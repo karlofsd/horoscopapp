61eba17d0cc283acefbe74463424cdd0eeb29ddb
Karlo Soberon <sober.karlo@gmail.com>
2024-01-24 21:44:21 -0500

    initial commit

---
+++ app/src/main/java/com/example/horoscopapp/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,22 @@
+package com.example.horoscopapp.ui.horoscope
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.horoscopapp.databinding.FragmentHoroscopeBinding
+
+class HoroscopeFragment : Fragment() {
+
+    private var _binding: FragmentHoroscopeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHoroscopeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/horoscopapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,24 @@
+package com.example.horoscopapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.horoscopapp.R
+import com.example.horoscopapp.databinding.FragmentPalmistryBinding
+
+
+class PalmistryFragment : Fragment() {
+    private var _binding: FragmentPalmistryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+}
